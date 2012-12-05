f8c780a34812d18f91c43da9ada573444f9021c8
Jensen <jensechu@gmail.com>
2012-12-05 13:31:20 -0800

    Now the js takes a title attr for a body part, and the image src for an image and gives it to the dressup function.

--- js/dressup.js
+++ js/dressup.js
@@ -2,10 +2,21 @@ $(document).ready(function() {
     
     // Retrieves the swatch cateogory from the unordered list's
     // title attribute and gives it to the switcher.
-    $('#swatches li ').click(function() {
-	var bodyPart = $(this).parent().attr("title");
-	console.log(bodyPart);
+    $('#swatches li').click(function() {
+	var swatchPart = $(this).parent().attr("title");
+	var swatchImage = $(this).children("img").attr("src");
+
+	console.log(swatchPart);
+	console.log(swatchImage);
+
+	dressup(swatchPart, swatchImage);
     });
+
+    // Uses the title and img attr of the swatch item to
+    // find the right body template div and change it's image src.
+    function dressup(swatchPart, swatchImage) {
+	$("#"+swatchPart+" > img").attr({"src": swatchImage});
+    }
 	
     
 
