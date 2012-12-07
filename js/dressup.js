cddb512e476a051a9057ce134fad2f7f28ae65cf
Jensen <jensechu@gmail.com>
2012-12-06 23:08:42 -0800

    Stacked the main body divs on top of each other.  Refactored js by putting all of the dressup mechanics in the dressup function to avoid repetitive code.
    TODO: z-index changing to layer clothing.

--- js/dressup.js
+++ js/dressup.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
-    
-    // Retrieves the swatch cateogory from the unordered list's
-    // title attribute and gives it to the switcher.
-    $('#swatches li').click(function() {
-	var swatchPart = $(this).parent().attr("title");
-	var swatchImage = $(this).children("img").attr("src");
-
-	console.log(swatchPart);
-	console.log(swatchImage);
-
-	dressup(swatchPart, swatchImage);
-    });
 
     // Uses the title and img attr of the swatch item to
     // find the right body template div and change it's image src.
-    function dressup(swatchPart, swatchImage) {
+    function dressup(swatch) {
+	var swatchPart = $(swatch).parent().attr("title");
+	var swatchImage = $(swatch).children("img").attr("src");
+
 	$("#"+swatchPart+" > img").attr({"src": swatchImage});
     }
+
+    // Swatch Button
+    $('#swatches li').click(function() {
+	dressup(this);
+    });
+
+    // Reset Button
+    // Takes imgs with class 'default' and sets them on the body.
+    $('#reset').click(function() {
+	
+    });
 	
     
 
